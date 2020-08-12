0d672a00ea2772613eaf4e3a0f48e37b41d6fa9d
Kevin Sullivan <sullivan.kevinj@gmail.com>
2020-08-12 04:44:56 +0000

    Refactoring

--- affine/real_affine_space.lean
+++ affine/real_affine_space.lean
@@ -5,12 +5,8 @@ import data.real.basic
 abbreviation real_vec := aff_vec ℝ
 abbreviation real_pt  := aff_pt  ℝ
 
-#check real_vec 3
-#check real_pt 3
-
 abbreviation r3_vec := real_vec 3
 abbreviation r3_pt  := real_pt  3
-
 
 universes u v w
 
@@ -32,43 +28,12 @@ structure vec_struct :=
 
 inductive Algebra  
 | affine_space (a : aff_struct)
-| nat_monoid --placeholder, what really goes here? E.g., for quantity of material
 | vector_space (v : vec_struct)
+| nat_monoid -- placeholder
 
 noncomputable def to_affine : ℕ → aff_struct := λ n,
     ⟨aff_pt ℝ n, ℝ, aff_vec ℝ n, real.field, aff_comm_group ℝ n, aff_vec_space ℝ n, aff_coord_is ℝ n⟩
 
 
-noncomputable def to_vector : ℕ → vec_struct := λ n, -- TBD
+noncomputable def to_vector : ℕ → vec_struct := λ n, 
     ⟨ℝ, aff_vec ℝ n, real.field, aff_comm_group ℝ n, aff_vec_space ℝ n⟩
-
-
-/-
-def ra3space : _ := _
-
-pt, field, vec
-
-pt = aff_pt K n
-vec = aff_vec K n
-instance : affine_space pt field vec := _
-
-phys_vec := (meters, vec)
-phys_pt := (meters, pt)
-
-(meters, vec) + (meters, pt) = (meters, pt_2)
-(meters, vec) + (meters, vec) = (meters, vec)
--/
-
-
-
-/-
-Bottom: look at math, then look at phys, how it fits into DSL
-Top down: 
-    - geom3d := geometric_euclidean_space("world", 3)
-    - geom3d.stdFrame()
-    - geom3d.vectorSpace()
-    - geom3d.points()
-    - someVec3 := vector(geom3d.vectorSpace, [-1,4,2], geom3d.stdFrame)
-    - geometric_euclidean_space("world", 3)
-    * eval(Lit_Geom_Expr "world" 3) -- i want a physical space
--/ 
