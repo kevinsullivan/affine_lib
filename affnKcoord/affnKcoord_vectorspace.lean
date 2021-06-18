33eea57e1d16ed329ad5bdff5f5527e429256c23
Kevin Sullivan <sullivan.kevinj@gail.com>
2021-06-18 16:22:29 +0000

    Small cleanups

--- affnKcoord/affnKcoord_vectorspace.lean
+++ affnKcoord/affnKcoord_vectorspace.lean
@@ -1,7 +1,7 @@
 import .affnK
-import linear_algebra.affine_space.affine_equiv
-import linear_algebra.matrix
-import linear_algebra.basis
+--import linear_algebra.affine_space.affine_equiv
+--import linear_algebra.matrix
+--import linear_algebra.basis
 import linear_algebra.std_basis
 import .affnKcoord_definitions
 
