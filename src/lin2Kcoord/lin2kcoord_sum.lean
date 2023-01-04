73caf576dc5a0465295b82515aa1ac3a08274840
Kevin Sullivan <sullivan.kevinj@gmail.com>
2023-01-04 19:13:31 +0000

    Cleanup. Deleted mk_finset stuff from lin2kcoord_sum.lean

--- src/lin2Kcoord/lin2kcoord_sum.lean
+++ src/lin2Kcoord/lin2kcoord_sum.lean
@@ -6,8 +6,9 @@ import tactic.linarith
 import .lin2kcoord
 
 open_locale direct_sum
--- ⨁ i, β i is the n-ary direct sum direct_sum. This notation is in 
--- the direct_sum locale, accessible after open_locale direct_sum
+/-
+⨁ i, β i is now available as the n-ary direct sum.
+ -/
 
 universes u 
 
@@ -23,11 +24,17 @@ variables
 
 /-
 Abbreviation for the type of the direct sum (think
-product) of n K × K modules.
+product) of n K × K modules. The variable i here is
+the index, selected from a finite set (fin) of size n.
+
+lin2k_sum : 
+  Π (K : Type u_1) 
+  [_inst_1 : inhabited K] 
+  [_inst_2 : field K], 
+  ℕ → 
+  Type u_1
 -/
 abbreviation lin2k_sum := ⨁(i : fin n), (λi, K × K) i
-
-#check lin2k_sum
 
 /-
 Proof (by typeclass instantiation) that such a direct
@@ -35,17 +42,13 @@ sum of K×K modules over K is also a module.
 -/
 instance : module K (lin2k_sum K n) := by apply_instance
 
--- Not sure this is used anywhere, and it's somewhat broken, so ignore
-
+-- Not sure this is used, and it's broken, so ignore
 -- def mk_finset (n : ℕ) : finset (fin n) := sorry
-
 -- def finsetn := mk_finset n
-
 -- def mk_linnk : (fin (n) → K × K) → lin2k_sum K n :=
 --   λv, 
 --   let val : Π (i : (↑(finsetn (n)) : set (fin (n)))), (λi, K × K) i.val := λi, v i in
 --   direct_sum.lmk K _ _ _ (val)
-
 -- def mk_linnk' : (fin (n) → K × K) → lin2k_sum K n :=
 --   λv, 
 --   let val : Π (i : (↑(finsetn (n)) : set (fin (n)))), (λi, K × K) i.val := λi, v i in
